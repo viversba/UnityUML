f3801e9dd70bf90b30ea0786464446f283480f7d
Nicolas Viveros <nviverosb@unal.edu.co>
2019-02-12 22:41:52 -0500

    Changed window title

--- Assets/DiagramEngine/Editor/DiagramEngineGUI.cs
+++ Assets/DiagramEngine/Editor/DiagramEngineGUI.cs
@@ -66,6 +66,7 @@ namespace DEngine.View{
         private void Run() {
 
             window = GetWindow<DiagramEngineGUI>();
+            window.titleContent.text = "DEngine";
             window.position = new Rect(200, 100, 1200, 800);
             window.minSize = new Vector2(200, 200);
 
